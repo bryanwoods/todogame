b32624d35be0daa9baef50c2a5b5e8b777b5cea0
Bryan Woods <bryanwoods4e@bryan-woodss-macbook-pro.local>
2009-06-17 14:33:37 -0400

    Adding autotest

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.root :controller => "sessions", :action => "new"
+  map.root :controller => "tasks", :action => "new"
 
   # See how all your routes lay out with "rake routes"
 
