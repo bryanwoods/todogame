7f12fe03056c13abac84ffce45dc1b5c2d4c365e
Bryan Woods <bryanwoods4e@bryan-woodss-macbook-pro.local>
2009-08-06 11:29:23 -0400

    Tasks index as root page

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.root :controller => "tasks", :action => "new"
+  map.root :controller => "tasks", :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
